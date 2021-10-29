236b9f4b0deaa9c7d04525c861f68b3222b4616a
pravin duplo <pravin@duplocloud.net>
2021-10-28 19:02:16 -0700

    ubunutu 20 NetworkAgent

--- AgentUbuntu20/README.md
+++ AgentUbuntu20/README.md
@@ -1,17 +1,21 @@
 
 
-### ubuntu
+### ubuntu 20   
 ```
- sudo ./Setup.sh
 
+curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/AgentPy3/Setup.sh
+chmod +x ./Setup.sh
+sudo bash ./Setup.sh
 
+systemctl daemon-reload
+service NetworkAgent status
+service NetworkAgent stop
+service NetworkAgent start
+service NetworkAgent status
 
-```
-### centos 7
-* Successfully created ami-00e03b2804b0c1cc7 from instance i-093f691e8d5d9b0ee.
-* us-west2 =  ami-00e03b2804b0c1cc7 
-```
- sudo ./Setup_Centos.sh
+tail -f /var/log/NetworkAgent.log
  
-
+#vi /lib/systemd/system/NetworkAgent.service  
+#vi /usr/local/src/AgentV2/NetworkAgentV2.py
 ```
+### centos  not tested py3 -- need some work in py file
--- AgentUbuntu20/Setup.sh
+++ AgentUbuntu20/Setup.sh
@@ -1,143 +1,130 @@
 #!/bin/bash
 
 #
-# Agent variables 
+# Agent variables
 #
 AGENT='NetworkAgentV2'
-INSTALL_DIR='/usr/local/src'
-UDPTUNNEL='udptunnelv1'
-AGENT_DIR='AgentV2'
-
-
-#
-# Step 1: Install Docker and setup docker bridge 
-#
-curl -sSL https://get.docker.com/ | sudo sh 
-
-options=`cat /etc/default/docker | grep bridge`
-echo $options 
-
-if [ -z "$options" ]; then
-   sudo sed -i 's#-H fd://#-H fd:// -H tcp://0.0.0.0:4243#' /lib/systemd/system/docker.service
-fi
-
-#if [ -z "$options" ]; then
-#    sudo bash -c 'echo DOCKER_OPTS=\"-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock -b=bridge0\" >> /etc/default/docker'
-#fi
-
-echo "==========================="
-echo "     Docker installed      " 
-echo "==========================="
-echo `sudo docker info` 
-
-echo "=================================="
-echo "     Installing Network Agent     "
-echo "=================================="
-#
-# Step 2: Check if Agent directory exists if not create the directory
-#
-if [ -z "$INSTALLDIR" ]; then
-   DAEMON_DIR="$INSTALL_DIR/$AGENT_DIR"
-   echo "Installing $AGENT in default location $DAEMON_DIR"
-else
-   DAEMON_DIR=$INSTALLDIR/$AGENT_DIR
-   echo "Installing $AGENT in $DAEMON_DIR"
-fi 
-
-if [ -d $DAEMON_DIR ]; then 
-   echo "$DAEMON_DIR directory exists"
-else 
-   echo "Creating $DAEMON_DIR"
-   mkdir $DAEMON_DIR
-fi 
-
-DAEMON_NAME="NetworkAgentV2"
-DAEMON="$DAEMON_DIR/NetworkAgentV2.py"
-PYTHON_PATH="$DAEMON_DIR/flask/bin"
 DAEMON_DEFAULT_FILE="/etc/default/$AGENT"
+DAEMON_NAME="NetworkAgentV2"
+DAEMON_DIR='/usr/local/src/AgentV2'
+PYTHON_PATH="$DAEMON_DIR/flask/bin"
+DAEMON="$DAEMON_DIR/NetworkAgentV2.py"
 
-#
-# Step 3: Check if Agent is running. Shutdown Agent before 
-# installing 
-# 
-echo "Check if $AGENT is running"
-
-if ps ax | grep -v grep | grep $AGENT > /dev/null
-then
-    pid=`pgrep -f "python $DAEMON"`
-    echo "$AGENT is running $pid Shutting down $AGENT before installation..."
-    sudo kill -9 $pid
-else
-    echo "$AGENT is not running"
+if [ -d $DAEMON_DIR ]; then
+   echo "$DAEMON_DIR directory exists"
 fi
 
-#
-# Step 3: Check if UDP Tunnel Daemon is running. Shutdown Dameon before 
-# installing 
-# 
-echo "Check if $UDPTUNNEL is running"
+py3Install () {
 
-if ps ax | grep -v grep | grep $UDPTUNNEL > /dev/null
-then
-    pid=`pgrep $UDPTUNNEL`
-    echo "$UDPTUNNEL is running $pid. Shutting down $UDPTUNNEL before installation..."
-    kill -9 $pid
-else
-    echo "$UDPTUNNEL is not running"
-fi
+   echo "Performing  Install "
+
+  if [ -f $DAEMON_DEFAULT_FILE ]; then
+     echo "Found $DAEMON_DEFAULT_FILE, removing and re-installing..."
+     sudo rm $DAEMON_DEFAULT_FILE
+     sudo touch $DAEMON_DEFAULT_FILE
+  fi
+
+  echo "DAEMON=$DAEMON" | sudo tee --append $DAEMON_DEFAULT_FILE > /dev/null
+  echo "DAEMON_DIR=$DAEMON_DIR" | sudo tee --append $DAEMON_DEFAULT_FILE > /dev/null
+  echo "PYTHON_PATH=$PYTHON_PATH" | sudo tee --append $DAEMON_DEFAULT_FILE > /dev/null
+
+  cat $DAEMON_DEFAULT_FILE
+  #######
+
+  # install os level also ??? as virtualenv has issues ... but having them on outside can corrupt
+  # pip install flask requests  python-pytun docker
+  # pip install --upgrade python-iptables
+
+  # install virtualenv flask
+  virtualenv flask
+  yes | flask/bin/pip install flask
+  yes | flask/bin/pip install requests
+  yes | flask/bin/pip install python-pytun
+  yes | flask/bin/pip install --upgrade python-iptables
+  yes | flask/bin/pip install docker
+
+   #########
+   cd $DAEMON_DIR
+   curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/AgentPy3/NetworkAgentV2.py
+   chmod a+x NetworkAgentV2.py
+   cat NetworkAgentV2.py
+
+   #########
+   cd /lib/systemd/system
+   echo $PWD
+   curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/AgentPy3/NetworkAgent.service
+   ######
+   ls -alt NetworkAgent.service
+   ls -alt $DAEMON_DIR
+   ######
+   sudo systemctl daemon-reload
+   sudo systemctl enable NetworkAgent.service
+   sudo systemctl start NetworkAgent.service
+   sudo systemctl status NetworkAgent.service &
+
+}
+
+centosInstall () {
+   echo "Performing Centos Install "
+   py3Install
+}
+
+ubuntuInstall () {
+   echo "Performing Ubuntu Install "
+   py3Install
+}
 
 
-cd $DAEMON_DIR
-rm -rf NetworkAgentV2.py
-rm -rf NetworkSetupV2.py
-rm -rf udptunnelv1.py
-rm -rf flask
+installDependancies () {
+
+  if [ "$OS" = "centos" ]; then
+    echo "Centos Installing docker"
+    sudo yum  update
+    sudo yum install -y git wget curl net-tools vim
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install docker-ce docker-ce-cli containerd.io
+
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    sudo systemctl status docker
+    sudo docker ps
+
+    echo "Centos Installing Container Management Service"
+    sudo yum   update
+    sudo yum  -q -y install bridge-utils
+    sudo yum  -q -y install python-dev
+    sudo yum  -q -y install python-pip
+    sudo yum  -q -y install python-virtualenv
+    sudo yum  -q -y install gcc
+
+  elif  [ "$OS" = "Ubuntu" ]; then
+    echo "Ubuntu Installing docker"
+    curl -sSL https://get.docker.com/ | sudo sh
+    echo "Ubuntu Installing Container Management Service"
+    sudo apt-get  update
+    sudo apt install -q -y  python3-dev python3-pip bridge-utils  python3-virtualenv gcc
+    ###
+    options=`cat /etc/default/docker | grep bridge`
+    echo $options
+    if [ -z "$options" ]; then
+       sudo sed -i 's#-H fd://#-H fd:// -H tcp://0.0.0.0:4243#' /lib/systemd/system/docker.service
+    fi
+  else
+      echo "Unknown OS=$OS VER=$VER "
+  fi
 
 
-#
-# Step 5: Fetch Agent code from the repository
-#
-curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/AgentV2/NetworkAgentV2.py
+}
 
-curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/AgentV2/NetworkSetupV2.py
-
-curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/AgentV2/udptunnelv1.py
-
-chmod a+x NetworkAgentV2.py
-chmod a+x udptunnelv1.py
-
-echo "Installing Container Management Service"
-sudo apt-get  update
-sudo apt-get -q -y install bridge-utils
-sudo apt-get -q -y install python-dev 
-sudo apt-get -q -y install python-pip
-sudo apt-get -q -y install python-virtualenv
-
-virtualenv flask
-yes | flask/bin/pip install flask
-yes | flask/bin/pip install requests
-yes | flask/bin/pip install python-pytun 
-yes | flask/bin/pip install --upgrade python-iptables
-yes | flask/bin/pip install docker
-
-echo "==========================="
-echo "     Agent installed      " 
-echo "==========================="
-echo `ls -l $DAEMON_DIR`
-
-
-if [ -f $DAEMON_DEFAULT_FILE ]; then 
-   echo "Found $DAEMON_DEFAULT_FILE, removing and re-installing..." 
-   sudo rm $DAEMON_DEFAULT_FILE 
-   sudo touch $DAEMON_DEFAULT_FILE
-fi 
-
-echo "DAEMON=$DAEMON" | sudo tee --append $DAEMON_DEFAULT_FILE > /dev/null 
-echo "DAEMON_DIR=$DAEMON_DIR" | sudo tee --append $DAEMON_DEFAULT_FILE > /dev/null
-echo "PYTHON_PATH=$PYTHON_PATH" | sudo tee --append $DAEMON_DEFAULT_FILE > /dev/null
 
 getOSType () {
-    if [ -f /etc/lsb-release ]; then
+
+    if [ -f /etc/centos-release ]; then
+          . /etc/os-release
+          OS=$ID
+          VER=$VERSION_ID
+    elif [ -f /etc/lsb-release ]; then
         . /etc/lsb-release
         OS=$DISTRIB_ID
         VER=$DISTRIB_RELEASE
@@ -153,57 +140,119 @@ getOSType () {
     fi
 }
 
-ubuntuInstall () {
-   echo "Performing Ubuntu Install "
-   cd /etc/init/
-   echo $PWD
-   curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/udptunnel.conf
-   curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/NetworkAgent.conf
-   sudo start NetworkAgent
-}
-
-ubuntu16PlusInstall () {
-   echo "Performing Ubuntu 16.04 or later releaseInstall "
-   cd /etc/systemd/system
-   echo $PWD
-   #curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/udptunnel.service
-   #curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/NetworkAgent.service
-   curl -H "Accept: application/vnd.github.v3.raw" -O -L https://api.github.com/repos/duplocloud/linuxagent/contents/Agent/NetworkAgent.service
-   #cp /home/merchantsameer2014/NetworkAgent.service /etc/systemd/system
-   sudo systemctl daemon-reload
-   sudo systemctl enable NetworkAgent.service
-   sudo systemctl start NetworkAgent.service
-}
 
 #
-# Get the Linux OS Type 
-#   Ubunutu: Use upstart script to start the daemon
-#   Debian: Use init.d service to start the daemon
-#   Fedora/Redhat: Use systemd
+# Step 0: get os type
 #
+
+
 getOSType
 
 #
-# Step 6 Setup the Network Agent Daemon and launch
+# Step 1: Install Docker and setup docker bridge
 #
+echo; echo;
+echo "--------------------------OS=$OS VER=$VER-------------------------------------0"
+echo "Step 1: Install Docker and setup docker bridge";
+echo "--------------------------OS=$OS VER=$VER-------------------------------------1"
+echo; echo;
+
+installDependancies
+
+
+
+echo "==========================="
+echo "     Docker installed      "
+echo "==========================="
+echo `sudo docker info`
+
+echo "=================================="
+echo "     Installing Network Agent     "
+echo "=================================="
+
+#
+# Step 2: Check if Agent directory exists if not create the directory
+#
+echo; echo;
+echo "--------------------------OS=$OS VER=$VER-------------------------------------1"
+echo "Step 2: Check if Agent directory exists if not create the directory";
+echo "--------------------------OS=$OS VER=$VER-------------------------------------2"
+echo; echo;
+
+mkdir -p $DAEMON_DIR
+echo "files in $DAEMON_DIR "
+ls -alt $DAEMON_DIR
+
+#
+# Step 3: Check if Agent is running. Shutdown Agent before
+# installing
+#
+echo; echo;
+echo "--------------------------OS=$OS VER=$VER-------------------------------------2"
+echo "Step 3: Check if Agent is running. Shutdown Agent before installing";
+echo "--------------------------OS=$OS VER=$VER-------------------------------------3"
+echo; echo;
+
+echo "Check if $AGENT is running"
+
+if ps ax | grep -v grep | grep $AGENT > /dev/null
+then
+    pid=`pgrep -f "python $DAEMON"`
+    echo "$AGENT is running $pid Shutting down $AGENT before installation..."
+    sudo kill -9 $pid
+else
+    echo "$AGENT is not running"
+fi
+
+#
+# Step 4: Check if UDP Tunnel Daemon is running. Shutdown Dameon before
+# installing
+#
+echo; echo;
+echo "---------------------------------------------------------------3"
+echo "Step 4: Check if UDP Tunnel Daemon is running. Shutdown Dameon before installing";
+echo "---------------------------------------------------------------4"
+echo; echo;
+
+echo "Check if $UDPTUNNEL is running"
+
+if ps ax | grep -v grep | grep $UDPTUNNEL > /dev/null
+then
+    pid=`pgrep $UDPTUNNEL`
+    echo "$UDPTUNNEL is running $pid. Shutting down $UDPTUNNEL before installation..."
+    kill -9 $pid
+else
+    echo "$UDPTUNNEL is not running"
+fi
+
+echo "cd $DAEMON_DIR "
+
+cd $DAEMON_DIR
+rm -rf NetworkAgentV2.py
+rm -rf flask
+ls -alt $DAEMON_DIR
+
+#
+# Step 5 Setup the Network Agent Daemon and launch
+#
+echo; echo;
+echo "--------------------------OS=$OS VER=$VER-------------------------------------5"
+echo "Step 5 Setup the Network Agent Daemon and launchy"
+echo "--------------------------OS=$OS VER=$VER-------------------------------------6"
+echo; echo;
+
+echo "OS=$OS VER=$VER"
 
 case $OS in
    "Ubuntu")
-      case $VER in 
-         14*)
-             ubuntuInstall
-             ;;
-         16*)
-             ubuntu16PlusInstall
-             ;;
-         17*)
-             ubuntu16PlusInstall
-             ;;
-      esac 
-      ;;
-   *)
+        ubuntuInstall
+        ;;
+    "centos")
+        centosInstall
+         ;;
+     *)
       echo "Unsupport OS: $OS, Version: $VER"
       ;;
 esac
 
-
+echo "--------------------------OS=$OS VER=$VER-------------------------------------6"
