e070b148e44a8777163a37f49d372280e99455f8
pravin duplo <pravin@duplocloud.net>
2021-12-06 10:58:21 -0800

    Amazon Linux2

--- AgentAmazonLinux2/Setup.sh
+++ AgentAmazonLinux2/Setup.sh
@@ -106,7 +106,7 @@ installDependancies () {
     echo "amzn Amazon Linux 2 Installing docker"
     sudo yum update -y
     sudo amazon-linux-extras install docker
-    sudo yum install docker
+    sudo yum install -y docker
     sudo service docker start
     sudo usermod -a -G docker ec2-user
     docker info
